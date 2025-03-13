5180fa0df3330a449a1f36dbd7c08595250a6bb7
Darshan Patil <darshan11patil3@gmail.com>
2025-03-14 00:04:30 +0530

    Add links in social media handles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
-import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+import { FaDiscord, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaBluesky, FaXTwitter } from "react-icons/fa6";
 
 const socialLinks = [
-  { href: "https://discord.com", icon: <FaDiscord /> },
-  { href: "https://twitter.com", icon: <FaTwitter /> },
-  { href: "https://youtube.com", icon: <FaYoutube /> },
-  { href: "https://medium.com", icon: <FaMedium /> },
+  { href: "https://discord.gg/ZTYuRWwF", icon: <FaDiscord /> },
+  { href: "https://x.com/D4rsh69", icon: <FaXTwitter /> },
+  { href: "https://bsky.app/profile/d4rsh.bsky.social", icon: <FaBluesky /> },
+  { href: "https://www.linkedin.com/in/darshan-patil-8b15aa230/", icon: <FaLinkedin /> },
+  { href: "https://github.com/DARSHAN-PATIL11", icon: <FaGithub /> },
 ];
 
 const Footer = () => {
@@ -12,7 +14,7 @@ const Footer = () => {
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          ©Nova 2024. All rights reserved
+          Copyright &copy; {new Date().getFullYear()}. All rights reserved.
         </p>
 
         <div className="flex justify-center gap-4  md:justify-start">
